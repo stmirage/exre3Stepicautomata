a6ae1af30e0f26148ea7257c1040b1862b677324
Nick Louzgarev <nik.luzgarev@yandex.ru>
2020-04-13 18:53:46 +0400

    Update test_items.py
    add time

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
